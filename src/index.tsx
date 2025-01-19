348ebbd3d05efa651d0c5759dc104c5f04fea5b9
Zaskier <162003201+EJ-Zaskier@users.noreply.github.com>
2025-01-19 01:07:40 -0600

    soluciones

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';  // Cambiar a 'react-dom/client' en lugar de 'react-dom'
 import App from './App.tsx';
 
-// Usar createRoot en lugar de render
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
+
--- src/services/meilisearch.js
+++ src/services/meilisearch.js
@@ -6,4 +6,15 @@ const client = new MeiliSearch({
   apiKey: 'c716781c75b1115ae1bd945fd73b87d2f12a5f2e878cfc6fbe45f68d57be'
 });
 
+const manejarBusqueda = async () => {
+  try {
+    const index = client.index('peliculas');
+    const results = await index.search('tu_query');
+    console.log(results);
+  } catch (error) {
+    console.error('Error al realizar la búsqueda:', error);
+  }
+};
+
+manejarBusqueda();
 export default client;
