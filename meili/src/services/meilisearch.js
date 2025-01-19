8d5196d7d954d737f1e6eef25a8097c62df14326
Zaskier <162003201+EJ-Zaskier@users.noreply.github.com>
2025-01-18 23:21:19 -0600

    conexion y creacion del proyecto

---
+++ meili/src/services/meilisearch.js
@@ -0,0 +1,9 @@
+import { MeiliSearch } from 'meilisearch';
+
+// Configura conexión con el servidor Meili
+const client = new MeiliSearch({
+  host: 'http://18.227.13.140:7700',
+  apiKey: 'c716781c75b1115ae1bd945fd73b87d2f12a5f2e878cfc6fbe45f68d57be'
+});
+
+export default client;
